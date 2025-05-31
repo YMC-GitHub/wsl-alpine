94f435041721d3763c59708bb3e9948d82965804
yemiancheng <ymc.github@gmail.com>
2025-05-31 08:26:59 +0800

    scripts(wsl2): use ping_network func

--- scripts/04-check-wsl-ip-and-ping.sh
+++ scripts/04-check-wsl-ip-and-ping.sh
@@ -7,6 +7,7 @@ check_result(){
     local flag_exit=$2
     local msg_success="✅"
     local msg_failed="❌"
+
     if [ $? -eq 0 ]; then
         echo "$msg_head $msg_success";
     else
@@ -14,25 +15,19 @@ check_result(){
         [ $flag_exit -eq 0 ] && exit 1;
     fi
 }
+ping_network(){
+    local count=$1;
+    # ping loop address in wsl:
+    ping -c $count 127.0.0.1 > /dev/null 2>&1
+    check_result "ping loop address in wsl" 0;
 
-# use emoji to show result:
-# if [ $? -eq 0 ]; then
-#     echo "✅";
-# else
-#     echo "❌";
-# fi
+    # ping wsl ip in wsl:
+    ping -c $count $wslip  > /dev/null 2>&1;
+    check_result "ping wsl ip in wsl" 1;
 
-# ping loop address in wsl:
-ping -c 2 127.0.0.1 > /dev/null 2>&1
-check_result "ping loop address in wsl" 0;
+    # ping internet in wsl:
+    ping -c $count www.baidu.com > /dev/null 2>&1;
+    check_result "ping internet in wsl" 1;
+}
 
-# ping wsl ip in wsl:
-ping -c 2 $wslip  > /dev/null 2>&1;
-check_result "ping wsl ip in wsl" 1;
-
-# ping internet in wsl:
-ping -c 2 www.baidu.com > /dev/null 2>&1;
-check_result "ping internet in wsl" 1;
-
-
-# check_result "ping internet in wsl"
+ping_network 3
