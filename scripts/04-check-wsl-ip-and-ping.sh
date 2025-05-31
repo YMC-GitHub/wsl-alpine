641085aef29692307fd841f1f4cd0786e5b9c618
yemiancheng <ymc.github@gmail.com>
2025-05-31 08:24:27 +0800

    scripts(wsl2): use emoji to show result

--- scripts/04-check-wsl-ip-and-ping.sh
+++ scripts/04-check-wsl-ip-and-ping.sh
@@ -5,18 +5,26 @@ echo $wslip;
 check_result(){
     local msg_head=$1
     local flag_exit=$2
+    local msg_success="✅"
+    local msg_failed="❌"
     if [ $? -eq 0 ]; then
-        echo "$msg_head success";
+        echo "$msg_head $msg_success";
     else
-        echo "$msg_head failed";
+        echo "$msg_head $msg_failed";
         [ $flag_exit -eq 0 ] && exit 1;
     fi
 }
 
+# use emoji to show result:
+# if [ $? -eq 0 ]; then
+#     echo "✅";
+# else
+#     echo "❌";
+# fi
+
 # ping loop address in wsl:
 ping -c 2 127.0.0.1 > /dev/null 2>&1
 check_result "ping loop address in wsl" 0;
-
 
 # ping wsl ip in wsl:
 ping -c 2 $wslip  > /dev/null 2>&1;
@@ -28,10 +36,3 @@ check_result "ping internet in wsl" 1;
 
 
 # check_result "ping internet in wsl"
-
-# use emoji to show result:
-# if [ $? -eq 0 ]; then
-#     echo "✅";
-# else
-#     echo "❌";
-# fi
